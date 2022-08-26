8410ba21adad14e47b31a3b280692776bd12d821
Anastasia Sytsevich <drozdushka@gmail.com>
2022-08-26 17:17:10 +0300

    feat: add function coloredDecks and function stagesDecks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 
-import {
-  brownCards,
-  blueCards,
-  greenCards
-}  from './data/mythicCards/index.js'
+// import {
+//   brownCards,
+//   blueCards,
+//   greenCards
+// }  from './data/mythicCards/index.js'
+
+import brownCardsData from './data/mythicCards/brown/index.js';
+import greenCardsData from './data/mythicCards/green/index.js';
+import blueCardsData from './data/mythicCards/blue/index.js';
 import ancientsData from './data/ancients.js'
 
-const testir = brownCards.cardsData[3];
-console.log(testir);
+// const testir = brownCardsData[3];
+// console.log(testir);
 
 const Azathoth = document.querySelector('.Azathoth');
 const Cthulhu = document.querySelector('.Cthulhu');
@@ -80,8 +84,9 @@ function showSumByColumn(matrix) {
     sumResults.push(sum);
   }
 console.log(sumResults);
-
+return sumResults;
 }
+
 showSumByColumn(matrixAzathoth);
 
 /* функция, которая добавляет значения
@@ -110,12 +115,127 @@ function addNumbers(matrix) {
   ShubNiggurath.addEventListener("click", () => addNumbers(matrixShubNiggurath));
 
 
-  /* функция, возвращающая 
+//  __________________________________________________________
+
+// собираю зеленую колоду на примере Азатота
+
+  // let deskLengthGreen = showSumByColumn(matrixAzathoth)[0]; // количество зеленых карт
+  // let min = 0;
+  // let max = greenCardsData.length - 1;
+
+  // функция которая собирает зеленую колоду
+
+// function addGreenDesk () {
+//   let result = [];
+//   for (let i=0; i < deskLengthGreen; i++) {
+//     if(!result.includes(greenCardsData[getRandomNum(min,max)])) {
+//       result.push(greenCardsData[getRandomNum(min,max)])
+//     }
+//   }
+//   console.log(result);
+// }
+// addGreenDesk();
+
+
+
+
+// функция которая собирает колоду по цветам, на примере Азатота
+
+const green = 0;
+const brown = 1;
+const blue = 2;
+
+const stage1 = 0;
+const stage2 = 1;
+const stage3 = 2;
+
+function addFirstDesk(matrix, cardsData, color) {
+
+  let deskLength = showSumByColumn(matrix)[color]; // количество карт
+  let min = 0;
+  let max = cardsData.length - 1;
+  let result = [];
+
+  while (result.length < deskLength) {
+    if (!result.includes(cardsData[getRandomNum(min, max)])) {
+      result.push(cardsData[getRandomNum(min, max)])
+    }
+  }
+  return result;
+}
+
+const firstGreenDesk = addFirstDesk(matrixAzathoth, greenCardsData, green);
+const firstBrownDesk = addFirstDesk(matrixAzathoth, brownCardsData, brown);
+const firstBlueDesk = addFirstDesk(matrixAzathoth, blueCardsData, blue);
+
+console.log(firstGreenDesk);
+console.log(firstBrownDesk);
+console.log(firstBlueDesk);
+
+ /* функция, возвращающая 
   рандомное значение из интервала
 */
 
-  function getRandomNum(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* делаем колоды для этапов
+*/
+
+
+/*функция которая выбирает нужное количество карт*/
+// function arrayFilled(len, cardsData) {
+//   let min = 0;
+//   let max = cardsData.length - 1;
+//   let array = [];
+//   for(len; len > 0; len--) {
+//     array.splice(cardsData[getRandomNum(min, max)]);
+//   }
+//   return array;
+// }
+
+// function stageDesk (firstDesk, matrix, color, stage) {
+//   let result =[];
+//   for (let i=0; i < matrix[stage][color]; i++) {
+//     result = firstDesk.pop();
+//   }
+//   return result;
+// }
+
+function stageDesk (firstDesk, matrix, color, stage) {
+  let result =[];
+  let x = [];
+  while (x.length < matrix[stage][color]) {
+    result = firstDesk.pop();
+    x.push(result);
   }
+ return x;
+}
+
+// while (arr.length > 0) { // массив уменьшается в цикле пока не достигнет нуля
+// 	let first = arr.shift();
+// 	let last  = arr.pop();
+	
+// 	let str = first + last; // тут будет строка '16', потом '25', потом '34'
+// 	result.push(str);
+// }
+
+
+// const test1 = stageDesk(firstBrownDesk, matrixAzathoth, brown, stage1);
+// console.log(test1);
+
+let arr =[];
+const firstStageDesk = arr.concat(stageDesk(firstGreenDesk, matrixAzathoth, green, stage1), stageDesk(firstBrownDesk, matrixAzathoth, brown, stage1), stageDesk(firstBlueDesk, matrixAzathoth, blue, stage1));
+const secondStageDesk = arr.concat(stageDesk(firstGreenDesk, matrixAzathoth, green, stage2), stageDesk(firstBrownDesk, matrixAzathoth, brown, stage2), stageDesk(firstBlueDesk, matrixAzathoth, blue, stage2));
+const thirdStageDesk = arr.concat(stageDesk(firstGreenDesk, matrixAzathoth, green, stage3), stageDesk(firstBrownDesk, matrixAzathoth, brown, stage3), stageDesk(firstBlueDesk, matrixAzathoth, blue, stage3));
+
+console.log(firstStageDesk);
+console.log(secondStageDesk);
+console.log(thirdStageDesk);
+
+// const test = stageDesk(firstGreenDesk, matrixAzathoth, green, stage1);
+// console.log(test);
+// console.log(firstGreenDesk);
